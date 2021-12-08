f8d0648688a8094f5cb0a5b0d165b50931bee72a
JB <JB@DESKTOP-L0JUVIS>
2021-12-08 23:27:18 +0000

    Add project files.

---
+++ EdApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace EdApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = new ShapeBucket<Red>();
+
+            sb.AddShape(new RedTriangle()); //yes
+            sb.AddShape(new RedCircle());  //yes
+            sb.AddShape(new BlueSquare());  //no
+
+        }
+    }
+
+    public class ShapeBucket<T>
+    {
+        public void AddShape<T2>(T2 colouredShape) where T2 : T
+        {
+            //do something
+        }
+    }
+
+    public class RedTriangle : Red { }
+    public class RedCircle : Red { }
+    public class BlueSquare : Blue  { }
+    
+
+    public class Red : Colour {}
+    public class Blue : Colour { }
+
+    public abstract class Colour : IShape { }
+
+    public interface IShape { }
+
+
+}
